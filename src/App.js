6eee66919c6afa665edbd67f9c3269d426241a5a
Daniel Lwetabe <dannylwe11@gmail.com>
2021-06-13 18:21:56 +0300

    simple app completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react'
-import {atom, useRecoilState} from 'recoil'
+import React from "react";
+import { atom, useRecoilState, useRecoilValue, selector } from "recoil";
 
 const counterState = atom({
   key: "counterState",
-  default: 0
+  default: 0,
+});
+
+const x2Selector = selector({
+  key: "x2Selector",
+  get: ({get}) => {
+    const count = get(counterState)
+    return count * 2
+  }
 })
 
 function App() {
-  const [count, setCount] = useRecoilState(counterState)
+  const [count, setCount] = useRecoilState(counterState);
+  const doubleCount = useRecoilValue(x2Selector)
 
   const increment = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   const decrement = () => {
-    setCount(count - 1)
-  }
+    setCount(count - 1);
+  };
   return (
     <div className="App">
       <p> Count: {count} </p>
+      <p> Double Count: {doubleCount} </p>
+      
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
